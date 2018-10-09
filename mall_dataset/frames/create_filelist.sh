41359560e3f436ef1fa3a7f79db3e20facb0a56e
guohaoyu110 <guohaoyu110@gmail.com>
2018-10-09 15:41:31 +0800

    new commit

--- mall_dataset/frames/create_filelist.sh
+++
@@ -1,8 +0,0 @@
-# /usr/bin/env sh 
-DATA=/Users/haoyuguo/Desktop/mall_dataset/frames
-echo "Create train.txt..."
-rm -rf $DATA/train.txt # 删除train.txt这个文件
-find $DATA -name *.jpg>>$DATA/train.txt
-cat $DATA/train.txt
-echo "Done.."
-
